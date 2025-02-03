bf30e72df4723b3531f06da2bf00ee7ce7cb6b82
ImJustNon <Im_Just_Non@hotmail.com>
2025-02-04 00:18:32 +0700

    Fix bg

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -7,6 +7,7 @@ import googleIconSVG from "../../../assets/google-icon.svg";
 import sbtvcAloneLogo from "../../../assets/sbtvc.jpg";
 import { useDisclosure } from "@chakra-ui/react";
 import LoginModal from "@/components/LoginModal";
+import login_bg from "../../../assets/login_bg.png";
 
 export default function Login(): React.JSX.Element {
     const loginModalDisclosure = useDisclosure();
@@ -17,18 +18,20 @@ export default function Login(): React.JSX.Element {
     return (
         <>
             <div className="flex flex-col grow">
-                <div className="flex flex-col grow items-center justify-center gap-5">
-                    <Image className="max-w-[60%] animate__animated animate__fadeIn animate__slow" src={sbtvc_logo} alt="sbtvc" />
+                <div className="flex flex-col grow items-center justify-center gap-5 bg-cover bg-no-repeat bg-center " style={{
+                    backgroundImage: `url(${login_bg.src})`,
+                }}>
+                    {/* <Image className="max-w-[60%] animate__animated animate__fadeIn animate__slow" src={sbtvc_logo} alt="sbtvc" />
                     <div className="text-center font-normal text-3xl text-[#f9682f] px-16 font-fchome animate__animated animate__fadeIn animate__slower">
                         {("ระบบเช็คชื่อกิจกรรมนักเรียน")}
                     </div>
                     <div className="text-center font-normal text-xl text-[#f9682f] px-5 font-fchome animate__animated animate__fadeIn animate__slower">
                         {("วิทยาลัยอาชีวศึกษาเทคโนโลยีฐานวิทยาศาสตร์ (ชลบุรี)")}
-                    </div>
+                    </div> */}
                 </div>
+
                 <div className="bg-gradient-to-b from-[#f76418] to-[#c74605] p-8 py-10 rounded-t-3xl">
-                    <div className="text-center text-white text-lg font-semibold mb-6">{""}</div>
-                    <div> {/*Google Login Button*/}
+                    <div> {/* Login Button*/}
                         <button className="w-full flex flex-row justify-center items-center gap-2 bg-white text-black py-3 rounded-xl hover:bg-[#e6e6e6] active:bg-[#cfcfcf] hover:text-[#f76418] duration-300 cursor-pointer hover:scale-105 active:scale-100" onClick={loginModalDisclosureOnOpen}>
                             <span>
                                 <Image className="h-10 w-fit" src={sbtvcAloneLogo} alt="google_signin" />
